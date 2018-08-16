70e6c4a43e25ffc8913a852e4613edce6a5b6497
FrancoDiogo <francodiogo797@gmail.com>
2018-08-16 16:12:24 -0300

    inclusao dos exercicios 1 e 2

--- src/main/java/br/com/db1/ExercicioDeColecao.java
+++ src/main/java/br/com/db1/ExercicioDeColecao.java
@@ -1,5 +1,22 @@
 package br.com.db1;
 
-public class ExercicioDeColecao {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ExercicioDeColecao {
+	
+	public List<String> exercicio1() {
+		List<String> cores = new ArrayList<String>();
+		cores.add("Azul");
+		cores.add("Preto");
+		cores.add("Branco");
+		return cores;
+	}
+	
+	public Integer exercicio2() {
+		List<String> nomes = new ArrayList<String>();
+		nomes.add("Diogo");
+		nomes.add("Franco");
+		return nomes.size();
+	}
 }
--- src/test/java/br/com/db1/ExercicioDeColecaoTest.java
+++ src/test/java/br/com/db1/ExercicioDeColecaoTest.java
@@ -1,5 +1,25 @@
 package br.com.db1;
 
-public class ExercicioDeColecaoTest {
+import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
+import org.junit.Test;
+
+public class ExercicioDeColecaoTest {
+	
+	ExercicioDeColecao exercicio = new ExercicioDeColecao();
+	
+	@Test
+	public void Exercicio1Test() {
+		List<String> cores = exercicio.exercicio1();
+		assertTrue(cores.get(0) == "Azul");
+		assertTrue(cores.get(1) == "Preto");
+		assertTrue(cores.get(2) == "Branco");	
+	}
+	
+	@Test
+	public void Exercicio2Test() {
+		assertTrue(2 == exercicio.exercicio2());
+	}
 }
